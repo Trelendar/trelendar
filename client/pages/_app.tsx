27bd20e7a2989d13fb25425bbb969b6db7479f81
hoangkui <hoangkui2001@gmail.com>
2022-09-05 21:59:51 +0700

    auth with google

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,16 @@
-import '../styles/globals.css'
-import { AppProps } from 'next/app'
-import React from 'react'
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+import React from "react";
+import { SessionProvider } from "next-auth/react";
+const MyApp: React.FC<AppProps> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default MyApp;
