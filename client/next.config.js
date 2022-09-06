e8793df45ed40bfca038e7ed7b3e85082cd57684
hoangkui <hoangkui2001@gmail.com>
2022-09-06 19:13:45 +0700

    update config next

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,10 @@
-const withImages = require('next-images')
+// const withImages = require('next-images')
 
-module.exports = withImages()
+// module.exports = withImages()
+//next.config.js file
+module.exports = {
+  webpack(config) {
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
+};
