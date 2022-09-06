c929287d3ab8b770b3b056281ce90048efae2514
hoangkui <hoangkui2001@gmail.com>
2022-09-06 19:06:48 +0700

    change app

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,10 @@
-import "../styles/globals.css";
-import { AppProps } from "next/app";
-import React from "react";
-import { SessionProvider } from "next-auth/react";
-const MyApp: React.FC<AppProps> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
+import React from 'react';
+import { SessionProvider } from 'next-auth/react';
+const MyApp: React.FC<AppProps> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={session} refetchInterval={0}>
       <Component {...pageProps} />
     </SessionProvider>
   );
