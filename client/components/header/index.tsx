d2400b72a51ba24894d23fea765fc68416baade9
thanh-long-nguyen <thanh-long-nguyen@cybozu.vn>
2023-04-26 00:20:36 +0700

    Add button setting for header

--- client/components/header/index.tsx
+++ client/components/header/index.tsx
@@ -5,8 +5,14 @@ import { FiSettings } from 'react-icons/fi';
 import Image from 'next/image';
 import { getSession, signOut, useSession } from 'next-auth/react';
 import { Avatar } from '@mui/material';
+import { useRouter } from 'next/router';
+
 const Header: React.FC = () => {
   const { data } = useSession();
+
+  const [isDropdonw, setIsDropdonw] = useState<boolean>(false);
+  const router = useRouter();
+
   return (
     <nav className="grid grid-cols-7 gap-4 bg-colorHome py-2 max-h-17 h-[10vh]">
       <Link href="" className="col-span-1 ">
@@ -41,6 +47,41 @@ const Header: React.FC = () => {
               Login
             </div>
           </Link>
+        </div>
+      )}
+
+      {data && (
+        <div className="col-start-7 pt-6">
+          <div className="relative inline-block text-left">
+            <div>
+              <button
+                className="inline-flex w-full justify-center px-2 pr-3 py-1 pt-[0.4rem] text-sm hover:scale-125 transition"
+                onClick={() => setIsDropdonw(!isDropdonw)}
+              >
+                <svg className="-mr-1 ml-2 h-7 w-7" viewBox="0 0 30 30">
+                  <FiSettings size={30} color="white" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className={'dropdown-fullname divide-y ' + (!isDropdonw ? 'hidden' : '')}>
+            <div className="py-1" onClick={() => router.push('/workspace')}>
+              <div className="text-gray-700 block px-4 py-2 text-sm hover:bg-slate-100">
+                My projects
+              </div>
+            </div>
+            <div className="py-1" onClick={() => router.push('/calendar')}>
+              <div className="text-gray-700 block px-4 py-2 text-sm hover:bg-slate-100">
+                My calendar
+              </div>
+            </div>
+          
+            <div className="py-1">
+              <div className="text-gray-700 block px-4 py-2 text-sm hover:bg-slate-100 rounded-md">
+                Logout
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </nav>
